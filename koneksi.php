118204464d85482e72f75cf4d8ef02be46ae8617
adhitt <112746818+adhitD@users.noreply.github.com>
2024-11-11 14:50:16 +0700

    membuat koneksi

---
+++ koneksi.php
@@ -0,0 +1,11 @@
+<?php
+$serverhost ="localhost"
+$nama = "root"
+$password = ""
+$database = "sistemAlumni"
+
+$conn = "new mysqli($serverhost, $nama, $password, $database);
+
+if(!$conn){
+echo("error");
+?>
