365074fc61246a8ff8a8396be710961a7684dbee
adhitt <112746818+adhitD@users.noreply.github.com>
2024-11-12 20:29:51 +0700

    npa lgi

--- layouts/navbar.php
+++ layouts/navbar.php
@@ -1,17 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-  
+
+<?php
+include './koneksi.php';
+
+?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <link rel="stylesheet" href="../assets/navbar.css">
+    <link rel="stylesheet" href="./assets/navbar.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title></title>
@@ -20,12 +16,7 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=person" />
     <style>
    /* Custom styles for dropdown */
-   .dropdown-menu {
-    display: none;
-   }
-   .dropdown:focus-within .dropdown-menu {
-    display: block;
-   }
+   
    .all {
     background: linear-gradient(#141e30, #243b55);
 	
@@ -42,37 +33,31 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </script>
 
-<nav class="p-4 all">
+<nav class="p-4 all fixed-top">
     <div class="container mx-auto flex justify-between items-center">
      <div class="text-white text-lg font-bold">
       WEB 
      </div>
      <div class="flex items-center">
-      <a class="text-white nb hover:text-white px-3" href="../web/beranda.php">
+      <a class="text-white nb hover:text-primary px-3" href="#beranda">
        beranda
       </a>
-      <a class="text-white nb hover:text-white px-3" href="../web/tentang.php">
+      <a class="text-white nb hover:text-primary px-3" href="#loker">
+      informasi loker
+      </a>
+      <a class="text-white nb hover:text-primary px-3" href="#kegiatan">
        kegiatan
       </a>
-      <a class="text-white nb hover:text-white px-3" href="../web/tabel_gang.php">
-       informasi loker
-      </a>
-      <a class="text-white nb hover:text-white px-3" href="../web/saya.html">
-       tentang
-      </a>
-      <a class="text-white nb hover:text-white px-3" href="#">
-       Login
-      </a>
-        
-    
+     <a class="text-white bg-warning rounded nb hover:text-primary px-3" href="#">
+     login
+     </a>
+      
+      
+     </li>
      </div>
     </div>
    </nav>
 
 
-</body>
-</html>
-
-    
 </body>
 </html>
